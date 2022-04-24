3fecc4a4567705f9ddfd5f159d58e03a9391a754
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-04-24 09:01:17 +0300

    draft

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
+// import preprocess from 'svelte-preprocess';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// preprocess: preprocess(),
+
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
